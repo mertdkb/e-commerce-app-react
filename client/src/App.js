1a4a32cc15593bf56231d38ad2ed38c3087ec757
mertdkb <dikbykmert@gmail.com>
2022-11-22 14:22:57 +0300

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Signin from './pages/Auth/Signin'
+import Signup from './pages/Auth/Signup'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div id="content">
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/signin' element={<Signin />}/>
+          <Route path='/signup' element={<Signup />}/>
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>
 }
 
 export default App;
---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './styles.module.css'
+import { Button } from '@chakra-ui/react'
+
+function index() {
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.left}>
+                <div className="logo">
+                    <Link to="/">LOGO</Link>
+                </div>
+
+                <ul className={styles.menu}>
+                    <li>
+                        <Link to="/">Products</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.right}>
+                <Link to="/signin">
+                    <Button colorScheme='blue'>Login</Button>
+                </Link>
+                <Link to="/signup">
+                    <Button colorScheme='gray'>Register</Button>
+                </Link>
+
+            </div>
+        </nav>
+    )
+}
+
+export default index
