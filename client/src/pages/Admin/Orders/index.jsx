109e16bfe285cf52c269938f200b175e6753061a
mertdkb <dikbykmert@gmail.com>
2022-11-29 01:08:38 +0300

    admin orders page refactored

--- client/src/pages/Admin/Orders/index.jsx
+++ client/src/pages/Admin/Orders/index.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 import { useQuery } from 'react-query'
 import { fetchOrders } from '../../../api'
-import {Table, Thead, Tbody, Tr, Th, Td, TableCaption, Text,}from '@chakra-ui/react'
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, Text, } from '@chakra-ui/react'
 
 function Orders() {
     const { isLoading, isError, error, data } = useQuery('admin:orders', fetchOrders);
 
-    if(isLoading) {
+    if (isLoading) {
         return <div>
             loading
         </div>
     }
 
-    if(isError) {
+    if (isError) {
         return <div>
             error {error.message}
         </div>
@@ -23,8 +23,26 @@ function Orders() {
         <div>
             <Text fontSize="2xl" p="5">Orders</Text>
 
-            <Table>
-                
+            <Table variant="simple">
+                <TableCaption>Order list</TableCaption>
+                <Thead>
+                    <Tr>
+                        <Th>User</Th>
+                        <Th>Address</Th>
+                        <Th isNumeric>Items</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {
+                        data.map((item) => (
+                            <Tr key={item._id}>
+                                <Td>{item.user.email}</Td>
+                                <Td>{item.adress}</Td>
+                                <Td isNumeric>{item.items.length}</Td>
+                            </Tr>
+                        ))
+                    }
+                </Tbody>
             </Table>
         </div>
     )
